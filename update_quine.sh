e75790558b86e855e761bc23da0d0b7503465ec5
Suzanne Soy <jsmaniac.github@suzanne.soy>
2021-10-08 01:10:07 +0100

    Fixed stuff

--- update_quine.sh
+++ update_quine.sh
@@ -5,6 +5,7 @@ set -euET -o pipefail
 directory="${1:-.}"
 
 temp_file="$(mktemp)"
+hexdump="$(mktemp)"
 
 if test -z "$directory" -o "$directory" = "-h" -o "$directory" = "--help"; then
   echo 'Usage: ./update-quine.sh [path/to/directory]'
@@ -18,8 +19,10 @@ sed -i -e 's/XXX_PLACE''HOLDER_START_XXX/&\n\/\/ XXX_PLACE''HOLDER_DELETE_XXX/' 
 # Delete everythinbe between the start marker and the end marker, both included
 sed -i -e '/XXX_PLACE''HOLDER_DELETE_XXX/,/XXX_PLACE''HOLDER_END_XXX/d' "$directory"/index.html
 
+# Generate template that will be used for the quine (var src1 = "the marker"; everything else unchanged)
+printf %s\\n 'var src1 = "XXX_PLACEHOLDER_XXX";' >> "$temp_file"
+
 # Generate contents to be put after the start marker
-xxd -ps < "$directory"/index.html | tr -d \\n | fold -w 160 | sed -e 's/.*''/"&" +/' | sed -e '1s/^/var src1 = /' | sed -e '$s/ +$/;\n/' >> "$temp_file"
 printf %s\\n 'var hexLineWidth = 160;'              >> "$temp_file"
 printf %s\\n 'function srcdirectory (index_html) {' >> "$temp_file"
 
@@ -34,8 +37,17 @@ printf %s\\n '}' >> "$temp_file"
 
 printf %s\\n '// XXX_PLACE''HOLDER_END_XXX' >> "$temp_file"
 
+# Add the generated contents after the start marker
 # TODO: escape $temp_file inside the sed command, or make sure the path doesn't need escpaing.
 sed -i -e '/XXX_PLACE''HOLDER_START_XXX/ r '"$temp_file" "$directory"/index.html
+
+# Generate hexdump of the file with the placeholder
+xxd -ps < "$directory"/index.html | tr -d \\n | fold -w 160 | sed -e 's/.*''/"&" +/' | sed -e '1s/^/var src1 = /' | sed -e '$s/ +$/;\n/' >> "$hexdump"
+# Replace the placeholder with the hexdump
+# TODO: escape $temp_file inside the sed command, or make sure the path doesn't need escpaing.
+sed -i -e '/var src1 = "XXX_PLACE''HOLDER_XXX";/ r '"$hexdump" "$directory"/index.html
+# Remove the line with the placeholder
+sed -i -e '/var src1 = "XXX_PLACE''HOLDER_XXX";/ d' "$directory"/index.html
 
 echo "The hash given by the page should be:"
 ipfs cid base32 "$(ipfs add --hidden -Qr "$directory")"
